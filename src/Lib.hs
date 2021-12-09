28702084ac10572882764ea992626d61169e73dc
Werozel <khaibulaev.mn@gmail.com>
2021-12-09 20:58:23 +0300

    Completed hdnlers logic

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,25 +12,41 @@ import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
 
-data Reponse = Reponse
+data ResponseScheme = ResponseScheme
   { operator   :: String
   , arguments  :: [Double]
-  , result     :: Double
-  , error      :: String
+  , result     :: Maybe Double
+  , error      :: Maybe String
   } deriving (Eq, Show)
 
-$(deriveJSON defaultOptions ''Reponse)
+$(deriveJSON defaultOptions ''ResponseScheme)
 
 startApp :: IO ()
-startApp = run 8080 app
+startApp = run 8000 app
 
-type API = Capture "action" String :> Capture "first" Double :> Capture "second" Double :> Get '[PlainText] String
+type ActionApi = Capture "action" String :> Capture "first" Double :> Capture "second" Double :> Get '[JSON] ResponseScheme
+type SqrtApi = "sqrt" :> Capture "value" Double :> Get '[JSON] ResponseScheme 
+
+type API = ActionApi :<|>  SqrtApi
 
 app :: Application
 app = serve (Proxy :: Proxy API) server
 
 server :: Server API
-server = handleAction
+server = handleAction :<|> handleSqrt
 
-handleAction :: String -> Double -> Double -> Handler String
-handleAction action first second = return (action ++ show first ++ show second)
+handleAction :: String -> Double -> Double -> Handler ResponseScheme
+handleAction "add" first second = return $ ResponseScheme "add" [first, second] (Just $ first + second) Nothing
+handleAction "sub" first second = return $ ResponseScheme "sub" [first, second] (Just $ first - second) Nothing
+handleAction "mul" first second = return $ ResponseScheme "mul" [first, second] (Just $ first * second) Nothing
+handleAction "div" first second 
+  | second == 0 = return $ ResponseScheme "div" [first, second] Nothing (Just "Division by zero")
+  | otherwise   = return $ ResponseScheme "div" [first, second] (Just $ first / second) Nothing
+handleAction "pow" first second
+  | first == 0 && second < 0 = return $ ResponseScheme "pow" [first, second] Nothing (Just "Negative power of zero")
+  | otherwise                = return $ ResponseScheme "pow" [first, second] (Just $ first ** second) Nothing
+
+handleSqrt :: Double -> Handler ResponseScheme
+handleSqrt value
+  | value < 0 = return $ ResponseScheme "sqrt" [value] Nothing (Just "Sqrt value less than 0")
+  | otherwise = return $ ResponseScheme "sqrt" [value] (Just $ sqrt value) Nothing
